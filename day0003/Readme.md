4d388a299e4698b1c028b72474a51fdffa9b751b
leeegun <leeegun@gmail.com>
2025-12-23 09:11:55 +0900

    Day 0003 - data types int and str

---
+++ day0003/Readme.md
@@ -0,0 +1,18 @@
+# Day 3
+
+## What I did
+
+- Learned about int and str
+- Checked data types with type()
+- Tried converting types
+
+## What I learned
+
+- Numbers and strings behave differently
+- Python does not mix types automatically
+- Types can be converted if needed
+
+## Thoughts
+
+- The result was different then I expected
+- Type seems important
---
+++ day0003/main.py
@@ -0,0 +1,14 @@
+#Day 3: Data type - int and str
+
+age = 10
+years = "10"
+
+print(age + 5)          # Integer addition
+print(years + "5")     # String concatenation
+
+print(type(age))      # Output: <class 'int'>
+print(type(years))    # Output: <class 'str'>
+
+print(str(age) + "5")      # Convert int to str
+print(int(years) + 5)      # Convert str to int
+# End of Day 3
