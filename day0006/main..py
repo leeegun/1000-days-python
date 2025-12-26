1e18b34fd0ac794338c6aabbb81e248cb8ece731
Your Name <you@example.com>
2025-12-26 21:34:59 +0900

    Day 0006 - for loop basics

---
+++ day0006/main..py
@@ -0,0 +1,14 @@
+# Day 0006 : for loop
+
+# for i in range(5):
+#    print("Day", i)
+
+# for day in range(1, 6):
+#    print("Day", day, "- Study Python")
+
+count = 1
+
+while count <= 3:
+    print("Count", count)
+    count += 1
+    
